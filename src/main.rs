e72eb1358736571c10f3a8998ad826a338772a66
Ben Hutchison <ben@aldaviva.com>
2022-11-22 03:50:23 -0800

    Make stdout text look more like that of the wake Node.js program I wrote. Exit without panicking because it looks nicer.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate core;
 
 use std::env;
 use std::fs::File;
+use std::process::exit;
 use std::str::FromStr;
 
 use serde_json::{Map, Value};
@@ -11,29 +12,48 @@ const CONFIG_FILENAME: &str = ".wake.json";
 fn main() {
     let mut config_file_absolute_path = env::home_dir().unwrap();
     config_file_absolute_path.push(CONFIG_FILENAME);
-    println!("Config file = {}", config_file_absolute_path.display());
 
     let config_file = File::open(config_file_absolute_path.as_path())
-        .unwrap_or_else(|e| { panic!("Failed to read configuration file {}: {}", config_file_absolute_path.display(), e) });
+        .unwrap_or_else(|e| {
+            eprintln!("Failed to read configuration file {}: {}", config_file_absolute_path.display(), e);
+            exit(1);
+        });
 
     let config_object: Value = serde_json::from_reader(config_file)
-        .unwrap_or_else(|e| { panic!("Could not parse configuration file JSON: {}", e) });
+        .unwrap_or_else(|e| {
+            eprintln!("Could not parse configuration file JSON: {}", e);
+            exit(1);
+        });
 
     let computer_name = env::args().nth(1)
-        .unwrap_or_else(|| { panic!("First argument must be a computer name") });
+        .unwrap_or_else(|| {
+            eprintln!("Usage: {} <computerName>", env::current_exe().unwrap().file_name().unwrap().to_str().unwrap());
+            exit(1);
+        });
 
     let computer: &Map<String, Value> = config_object[&computer_name].as_object()
-        .unwrap_or_else(|| { panic!("Configuration object in JSON file {} does not contain a {} key", config_file_absolute_path.display(), &computer_name) });
+        .unwrap_or_else(|| {
+            eprintln!("Configuration object in JSON file {} does not contain a {} key", config_file_absolute_path.display(), &computer_name);
+            exit(1);
+        });
 
-    println!("Waking up {}", &computer["label"].as_str().get_or_insert(&computer_name));
+    println!("Waking up {}...", &computer["label"].as_str().get_or_insert(&computer_name));
 
-    for str in computer["macAddresses"].as_array().unwrap() {
-        let mac_address_string = str.as_str().unwrap();
+    for mac_address_entry in computer["macAddresses"].as_array().unwrap() {
+        let mac_address_string = mac_address_entry.as_str()
+            .unwrap_or_else(|| {
+                eprintln!("entry in macAddresses array is not a string: {}", mac_address_entry);
+                exit(1);
+            });
 
         let mac_address: wol::MacAddr = wol::MacAddr::from_str(mac_address_string)
-            .unwrap_or_else(|e| { panic!("Invalid MAC address {}: {}", &mac_address_string, e) });
+            .unwrap_or_else(|e| {
+                eprintln!("Invalid MAC address {}: {}", &mac_address_string, e);
+                exit(1);
+            });
 
         wol::send_wol(mac_address, None, None).unwrap();
         println!("Sent wake-on-LAN packet to {}", &mac_address_string);
     }
+
 }
